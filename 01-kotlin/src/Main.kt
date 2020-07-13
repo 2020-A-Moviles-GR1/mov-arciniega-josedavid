541d93b18c8d7a4a61a39c6ea7b56196f75f9db4
Jose David <jose@epn.edu.ec>
2020-07-12 23:53:44 -0500

    Operadores: any, all, reduce, fold

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -68,7 +68,7 @@ fun main(args:Array<String>){
     println(resultado)
 
     val arregloConstante: Array<Int> = arrayOf(1,2,3)
-    val arregloDeListas: ArrayList<Int> = arrayListOf(30,31,32,33,20)
+    val arregloDeListas: ArrayList<Int> = arrayListOf(30,31,32,33,20) //
 
     print(arregloDeListas)
     arregloDeListas.add(24)
@@ -78,7 +78,7 @@ fun main(args:Array<String>){
 
     // OPERADORES / funciones puras : se encunetran en todos los lenguajes
 
-    // 1) forEach no devuelve nada = Unit , nos ayuda a iterar un arreglo
+    // 1) .forEach no devuelve nada = Unit , nos ayuda a iterar un arreglo
 
     arregloDeListas
             .forEach {
@@ -102,16 +102,14 @@ fun main(args:Array<String>){
                 println("valor Iteracion: $iteracion")
                 println("vañor con -1 = ${iteracion * -1}")
             }
-    // 2) map -> Muta el arreglo (Cambia el arreglo)
+    // 2) .map -> Muta el arreglo (Cambia el arreglo)
     // Enviamos el nuevo valor de la iteracion
     // Nos devuelve Un Nuevo Arreglo con los valores madificados
 
     arregloDeListas.map {it * -1}
 
     val respuestaMap1= arregloDeListas
-            .map {iterador: Int ->
-               iterador * -1
-            }
+            .map {it * -1}
 
     // retur@map puedo añadir mas de una linea de código y me devuelve cualquier cosa que necesite
 
@@ -146,12 +144,14 @@ fun main(args:Array<String>){
 
     // PROGRAMACION FUNCIONAL ME DEVUELVE UN NUEVO ARREGLO
 
-    // 3) filter -> FILTRA EL ARREGLO : no le modifica al arreglo,
+    // 3) .filter -> FILTRA EL ARREGLO : no le modifica al arreglo,
     //  Me devuelve una expresión (TRUE o FALSE)
     // ME devuelve un NUEVO arreglo que cumpla esa expresión
+
+    arregloDeListas.filter {it>25}
+
     val respuestaFilter1= arregloDeListas
-            .filter {iterador: Int -> iterador>25
-            }
+            .filter {it>25}
 
     val respuestaFilter2= arregloDeListas
             .filter {
@@ -164,20 +164,88 @@ fun main(args:Array<String>){
     println(respuestaFilter1)
     println(respuestaFilter2)
 
-    // 4)
-    // 5)
-    //
+    // 4) .any return TRUE o FALSE
+    // un nuevo arreflo que cumpla la expreción
+    // ANY -> OR (Some) = si almenos uno cumple -> TRUE
 
+    arregloDeListas.any {it > 33}  // ALMENOS uno debe cumplir la condicion para que retorne TRUE
+
+    val respuestaAny1= arregloDeListas
+            .any {it<33}
+
+    val respuestaAny2= arregloDeListas
+            .any {
+                iterador: Int ->
+                return@any iterador < 18
+            }
+
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaAny1)
+    println(respuestaAny2)
+
+    // 5) all return TRUE o FALSE
+    // un nuevo arreflo que cumpla la expreción
+    // ALL -> AND (Every) = TRUE, todo lo demas es falso
+
+
+    arregloDeListas.all {it>25}// TODOS deben cumplir la condición para devolver TRUE
+
+    val respuestaAll1= arregloDeListas
+            .all {it>25}
+
+    val respuestaAll2= arregloDeListas
+            .all{ iterador: Int ->
+                return@all iterador < 18
+            }
+
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaAll1)
+    println(respuestaAll2)
+
+
+    // 6) reduce
+    // Devuelve el acumulado
+    // Definimos en que valor empieza
+
+
+    val respuestaReduce= arregloDeListas // acumulador empieza en 0 o en VACIO con Strings
+            .reduce { acumulador, iteracion ->
+                return@reduce acumulador - iteracion
+            }
+
+
+
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaReduce)
+
+    // 7) fold
+    // foldRignt empieza desde la derecha
+    // Indico el valor desde donde empieza
+
+    val respuestaFold= arregloDeListas // acumulador empieza en 0 o en VACIO con Strings
+            .fold(
+                100,
+                { acumulador, iteracion ->
+                    return@fold acumulador + iteracion
+                }
+            )
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaFold)
+
+    // .forEach -> Nada/Unit
+    // .map -> Arreglo
+    // .filter -> Arreglo
+    // .any -> Bolleano
+    // .all -> Booleano
+    // .reduce -> Valor
+    // .fold -> Valor
 
 
 }
-
-
-
-
-
-
-
 
 
 fun calcularSueldo(
