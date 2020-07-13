16239149f5076057fc4a58cf9264f8e0613d766d
Jose David <jose@epn.edu.ec>
2020-07-12 22:19:19 -0500

    Operadores
    forEach, map, filter

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main(args:Array<String>){
     println("Hola")
@@ -19,21 +20,24 @@ fun main(args:Array<String>){
     edadAlumno1 = 23
     // Nombre  de veriables debe ser descriptivo
 
-    // VARIABLES Inmutables -> val (RECOMENDADO)
+    // VARIABLES INMUTABLES-> val (RECOMENDADO)
     val numeroCuenta = 1234543 // NO SE PUEDE REASIGNAR
     // numeroCuenta = 1234
 
     val fechaNacimiento = Date() // instanciamos clases dentro de Koltlin SIN new
 
 
+    //TIPOS DE VARIABLE
     // Variables declarando el tipo de variable
-    val nombreProfesor: String = "Vicente Adrian" //
+
+    val nombreProfesor1: String = "Vicente Adrian" //
+    val nombreProfesor2 = "Vicente Adrian" //(RECOMENDADO)
     val nombreAlumno = "Jose David"
     val sueldo: Double = 13.32
     val apellidoProfesor: Char = 'a'
 
     //  Variables SIN declarar el tipo de variable (RECOMENDADO)
-    val nombreProfesor1 = "Vicente Adrian1" //
+    val nombreProfesor = "Vicente Adrian1" //
     val nombreAlumno1 = "Jose David"
     val sueldo1 = 13.32
     val apellidoProfesor1 = 'a'
@@ -43,6 +47,7 @@ fun main(args:Array<String>){
     } else {
 
     }
+
     when (sueldo) {
         13.32 -> println("Sueldo Normal")
         -13.32 -> println("Sueldo Negativo")
@@ -55,20 +60,132 @@ fun main(args:Array<String>){
     //calcularSueldo(1000.00,14.00)       //como escribo
     calcularSueldo(1000.00,14.00) // como el editor me muestra
     calcularSueldo(
-            tasa=16.00,
-            sueldo=800.00
-    )//Named Parameters -> parametros nombrados
+            tasa=16.00,  //Named Parameters -> parametros nombrados
+            sueldo=800.00  //Named Parameters -> parametros nombrados
+    )
     calcularSueldo(700.00) // no me pide tasa por que taasa tiene valor por defecto
     var resultado = calcularSueldo(sueldo=650.00)
     println(resultado)
+
+    val arregloConstante: Array<Int> = arrayOf(1,2,3)
+    val arregloDeListas: ArrayList<Int> = arrayListOf(30,31,32,33,20)
+
+    print(arregloDeListas)
+    arregloDeListas.add(24)
+    print(arregloDeListas)
+    arregloDeListas.remove(element = 31)
+    print(arregloDeListas)
+
+    // OPERADORES / funciones puras : se encunetran en todos los lenguajes
+
+    // 1) forEach no devuelve nada = Unit , nos ayuda a iterar un arreglo
+
+    arregloDeListas
+            .forEach {
+                println("Valor de la iterracion: $it")
+            }
+
+    arregloDeListas
+            .forEach{ valorIteracion:Int ->
+                println("valor Iteracion: $valorIteracion")
+            }
+
+    val respuestaArregloForEach = arregloDeListas // forEach no devuelve nada -> Unit
+            .forEachIndexed{ index:Int, it:Int ->
+                println("valor Iteracion: $it")
+            }
+
+    println(respuestaArregloForEach) // void Unit
+
+    arregloDeListas
+            .forEach{ iteracion:Int ->
+                println("valor Iteracion: $iteracion")
+                println("vañor con -1 = ${iteracion * -1}")
+            }
+    // 2) map -> Muta el arreglo (Cambia el arreglo)
+    // Enviamos el nuevo valor de la iteracion
+    // Nos devuelve Un Nuevo Arreglo con los valores madificados
+
+    arregloDeListas.map {it * -1}
+
+    val respuestaMap1= arregloDeListas
+            .map {iterador: Int ->
+               iterador * -1
+            }
+
+    // retur@map puedo añadir mas de una linea de código y me devuelve cualquier cosa que necesite
+
+    val respuestaMap2= arregloDeListas
+            .map {iterador: Int ->
+                val primerValor = iterador * -1
+                val segundoValor = primerValor * 2
+                return@map segundoValor
+            }
+
+    val respuestaMap3= arregloDeListas
+            .map {iterador: Int ->
+                val primerValor = iterador * -1
+                val segundoValor = primerValor * 2
+                return@map segundoValor.toString()
+                //return@map true
+                // return@map Date()
+            }
+
+    val respuestaMap4= arregloDeListas
+            .map {iterador: Int ->
+                val primerValor = iterador * -1
+                val segundoValor = primerValor * 2
+                return@map Date()
+            }
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaMap1)
+    println(respuestaMap2)
+    println(respuestaMap3)
+    println(respuestaMap4)
+
+    // PROGRAMACION FUNCIONAL ME DEVUELVE UN NUEVO ARREGLO
+
+    // 3) filter -> FILTRA EL ARREGLO : no le modifica al arreglo,
+    //  Me devuelve una expresión (TRUE o FALSE)
+    // ME devuelve un NUEVO arreglo que cumpla esa expresión
+    val respuestaFilter1= arregloDeListas
+            .filter {iterador: Int -> iterador>25
+            }
+
+    val respuestaFilter2= arregloDeListas
+            .filter {
+                iterador: Int ->
+                val esMayorA25 = iterador > 25
+                return@filter esMayorA25
+            }
+    println("///////////////////////////////////////////")
+    println(arregloDeListas)
+    println(respuestaFilter1)
+    println(respuestaFilter2)
+
+    // 4)
+    // 5)
+    //
+
+
+
 }
+
+
+
+
+
+
+
 
 
 fun calcularSueldo(
         sueldo: Double, // Requeridos
         tasa: Double = 12.00, // Requeridos (Tienen valor por defecto)
-        calculoEspecial : Int? = null // Puede ser nulo "NULL"
-): Double {
+        calculoEspecial : Int? = null // Puede ser nulo "NULL" -> NO REQUERIDO
+
+): Double ? {  // El valor que devuelve Puede ser NULO
     if (calculoEspecial != null) {
         return sueldo * tasa * calculoEspecial
     } else {
